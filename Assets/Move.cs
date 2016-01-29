3569cd804943e11eb6ddb35b4dd6683bc832eeed
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2016-01-29 22:08:54 +0100

    Add hand with basic movement

---
+++ Assets/Move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public string horizontalAxis = "Horizontal";
+	public string verticalAxis = "Vertical";
+	public float force = 1;
+
+    void FixedUpdate() {
+		float x = Input.GetAxis (horizontalAxis);
+		float y = Input.GetAxis (verticalAxis);
+		Vector3 offset = new Vector3 (x, y, 0) * Time.fixedDeltaTime * force;
+		transform.position = transform.position + offset;
+    }
+}
