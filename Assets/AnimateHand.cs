8f14796c13f6a6a945f24ddb168f18e695b21761
monkeybits <kristianselvaag@gmail.com>
2016-01-30 16:27:24 +0100

    Added animation script for hand

---
+++ Assets/AnimateHand.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateHand : MonoBehaviour
+{
+    private Animation anim;
+
+    // This setup only works with:
+    //  Import --> Rig --> Animation Type  = Legacy
+    //  Import --> Animations --> Wrap mode = Loop
+
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+    void Update()
+    {
+        // Testing crossfade
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            anim.CrossFade("HighFive", 0.2f);
+            Debug.Log("Q down");
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            anim.CrossFade("FistHorizontal", 0.2f);
+            Debug.Log("Q up");
+        }
+
+        // Testing blending
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.Blend("GunFinger", 1.0f, 0.3f);
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            anim.Blend("GunFinger", 0.0f, 0.3f);
+        }
+    }
+}
