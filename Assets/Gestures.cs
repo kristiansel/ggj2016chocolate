14335f6c729fd3cddc4a1ade4af5c34363310fe4
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2016-01-30 21:49:18 +0100

    Add Gestures enum

---
+++ Assets/Gestures.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Gestures {
+	Handshake,
+	HorizontalFistBump,
+	VerticalFistBump,
+	FistHammerLeftTop,
+	FistHammerRightTop,
+	HighFive,
+	DownLowLeftTop,
+	DownLowRightTop,
+	//ExplodingFistBump
+}
