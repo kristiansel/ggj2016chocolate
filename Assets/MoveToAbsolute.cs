509f4bc6875ae9c84790640a8e76b224699103f0
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2016-01-30 01:06:52 +0100

    Use awesome hand model, lock rotation

--- Assets/MoveToAbsolute.cs
+++ Assets/MoveToAbsolute.cs
@@ -6,9 +6,13 @@ public class MoveToAbsolute : MonoBehaviour {
 	public string horizontalAxis = "Horizontal";
 	public string verticalAxis = "Vertical";
 	public float maxRadius = 5;
+	public bool flipHorizontalAxis = false;
 
 	void FixedUpdate() {
 		float x = Input.GetAxis (horizontalAxis);
+		if (flipHorizontalAxis) {
+			x *= -1;
+		}
 		float y = Input.GetAxis (verticalAxis);
 		transform.localPosition = new Vector3 (x, y, 0).normalized * maxRadius;
 	}
