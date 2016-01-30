bdc33f65feca6044a15832d6182304549a714655
Magnus Gohn Selvaag <magnusgohnselvaag@Magnuss-MBP.lan>
2016-01-30 23:50:48 +0100

    La til partikler og lyd når kollisjon blir kringkastet

---
+++ Assets/CollisionEventAudio.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionEventAudio : MonoBehaviour {
+	public AudioClip[] audioClips;
+	AudioSource audio;
+	int num = 0;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+
+		Messenger.AddListener<BroadcastCollision, BroadcastCollision, Vector2>(Events.Collision, OnHandsCollideAudio);
+	}
+		
+	// Update is called once per frame
+	void OnHandsCollideAudio (BroadcastCollision b1, BroadcastCollision b2, Vector2 v) {
+		if (audio.isPlaying) {
+			return;
+		}	
+
+		audio.clip = audioClips [num % audioClips.Length];
+		audio.Play ();
+
+		num++;
+	}
+}
