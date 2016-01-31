ed70327521dbd56bd676d2a66a173e02d3c3b212
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2016-01-31 16:05:41 +0100

    Play sound effects on successful gestures

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Audio : MonoBehaviour {
+	
+	public AudioClip[] successClips;
+	private AudioSource source;
+
+	void Start () {
+		source = GetComponent<AudioSource> ();
+		Messenger.AddListener (Events.CorrectGesture, HandleCorrectGesture);
+	}
+	
+	void HandleCorrectGesture () {
+		var index = (int)(Random.value * successClips.Length);
+		source.PlayOneShot (successClips [index]);
+	}
+}
