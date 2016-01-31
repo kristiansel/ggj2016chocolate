eed69cb2ed60bf446d8061eefe6bc3bc626880d5
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2016-01-31 16:41:29 +0100

    Add bump sound effects

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -5,15 +5,23 @@ using System.Collections;
 public class Audio : MonoBehaviour {
 	
 	public AudioClip[] successClips;
+	public AudioClip[] bumpClips;
 	private AudioSource source;
 
 	void Start () {
 		source = GetComponent<AudioSource> ();
 		Messenger.AddListener (Events.CorrectGesture, HandleCorrectGesture);
+		Messenger.AddListener<BroadcastCollision, BroadcastCollision, Vector2> (Events.Collision, HandleCollision);
 	}
 	
 	void HandleCorrectGesture () {
 		var index = (int)(Random.value * successClips.Length);
 		source.PlayOneShot (successClips [index]);
 	}
+
+	void HandleCollision (BroadcastCollision b1, BroadcastCollision b2, Vector2 velocity) {
+		var index = (int)(Random.value * bumpClips.Length);
+		source.PlayOneShot (bumpClips [index]);
+	}
+
 }
