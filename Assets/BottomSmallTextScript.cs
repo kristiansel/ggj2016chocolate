0ac4bb7d69d3c9a85c7e871722b9aebdf8dc3619
Kristian Selvaag <kristianselvaag@gmail.com>
2016-02-01 21:06:00 +0100

    bottom small text displays random hit messages

--- Assets/BottomSmallTextScript.cs
+++ Assets/BottomSmallTextScript.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class BottomSmallTextScript : MonoBehaviour {
 
+	private Text textObject;
+
 	// Use this for initialization
 	void Start () {
-	
+		Messenger.AddListener(Events.CorrectGesture, HandleCorrectGesture); // add argument to this callback indicating qesture quality
+		gameObject.SetActive (false);
+		textObject = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	private static string[] qualityMessages = {
+		"Epic fail!",
+		"Fail!",
+		"Awkward",
+		"Decent",
+		"Nice",
+		"Awsome!",
+		"Sick!"
+	};
+		
+	void HandleCorrectGesture()
+	{
+		gameObject.SetActive (true);
+
+		int message = (int)Random.Range (0, qualityMessages.Length);
+		textObject.text = qualityMessages[message];
 	}
 }
