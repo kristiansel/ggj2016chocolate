ae961b190255adbdd0652451cea8bf4e53cf3f1d
Magnus Gohn Selvaag <magnusgohnselvaag@Magnuss-MBP.lan>
2016-02-07 23:05:19 +0100

    La til lyder ved feil gesture

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -5,18 +5,26 @@ using System.Collections;
 public class Audio : MonoBehaviour {
 	
 	public AudioClip[] successClips;
+	public AudioClip[] failClips;
 	public AudioClip[] bumpClips;
+
 	private AudioSource source;
 
 	void Start () {
 		source = GetComponent<AudioSource> ();
 		Messenger.AddListener (Events.CorrectGesture, HandleCorrectGesture);
+		Messenger.AddListener (Events.IncorrectGesture, HandleIncorrectGesture);
 		Messenger.AddListener<BroadcastCollision, BroadcastCollision, Vector2> (Events.Collision, HandleCollision);
 	}
 	
-	void HandleCorrectGesture () {
+	void HandleCorrectGesture() {
 		int index = (int)Random.Range (0, successClips.Length);
 		source.PlayOneShot (successClips [index]);
+	}
+
+	void HandleIncorrectGesture() {
+		int index = (int)Random.Range (0, failClips.Length);
+		source.PlayOneShot (failClips [index]);
 	}
 
 	void HandleCollision (BroadcastCollision b1, BroadcastCollision b2, Vector2 velocity) {
