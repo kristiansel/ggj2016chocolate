f5ffdc2bb8bbcf21c2fa267fea6d18a0e7bb4cd6
Magnus Gohn Selvaag <magnusgohnselvaag@Magnuss-MBP.lan>
2016-02-08 00:10:18 +0100

    La til musikk, startet på bromance-effekter

--- Assets/Combo.cs
+++ Assets/Combo.cs
@@ -2,16 +2,26 @@
 using System.Collections;
 
 public class Combo : MonoBehaviour {
+	private GameObject spillernummeren;
+	private GameObject spillernummerto;
 	private int streak = 0;
 
 	// Use this for initialization
 	void Start () {
+		spillernummeren = GameObject.Find ("Player 1");
+		spillernummerto = GameObject.Find ("Player 2");
+
 		Messenger.AddListener (Events.CorrectGesture, HandleCombo);
 		Messenger.AddListener (Events.IncorrectGesture, HandleComboBreak);
 	}
 
 	void HandleCombo() {
 		streak += 1;
+
+		if (streak % 5 == 0) {
+			spillernummeren.GetComponentInChildren<ParticleSystem> ().Play ();
+			spillernummerto.GetComponentInChildren<ParticleSystem> ().Play ();
+		}
 	}
 
 	void HandleComboBreak() {
---
+++ Assets/MusicScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicScript : MonoBehaviour {
+	private AudioSource lyd;
+	void Start () {
+		lyd = GetComponent<AudioSource> ();
+		Messenger.AddListener (Events.FreestyleTriggered, HandleFreestyleTriggered);
+	}
+
+	void HandleFreestyleTriggered() {
+	}
+}
