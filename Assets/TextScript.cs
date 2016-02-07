472e8e2d3fa5de6aae3544d409125321c93d6167
Magnus Gohn Selvaag <magnusgohnselvaag@Magnuss-MBP.lan>
2016-02-07 09:34:59 +0100

    La til effekter på teksten, og la til et spillobjekt ved navn combotracker

---
+++ Assets/TextScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SuccessTextScript : MonoBehaviour {
+
+	private Rigidbody2D aiai;
+	private Text text;
+
+	void Start() {
+		text = GetComponent<Text> ();
+		aiai = GetComponent<Rigidbody2D> ();
+
+		Vector2 dytt = new Vector2 (Random.Range (-300.0f, 300.0f),
+			               			Random.Range (-300.0f, 300.0f));
+		
+		aiai.AddTorque(Random.Range(-300.0f, 300.0f));
+		aiai.AddForce (dytt, ForceMode2D.Impulse);
+	}
+}
