c699a58af155365aa04d63f1b52266964338df4c
Magnus Gohn Selvaag <magnusgohnselvaag@Magnuss-MBP.lan>
2016-02-08 00:49:37 +0100

    musikk spilles bare i freestyle mode

--- Assets/MusicScript.cs
+++ Assets/MusicScript.cs
@@ -1,13 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class MusicScript : MonoBehaviour {
 	private AudioSource lyd;
+
 	void Start () {
 		lyd = GetComponent<AudioSource> ();
 		Messenger.AddListener (Events.FreestyleTriggered, HandleFreestyleTriggered);
+		// Messenger.AddListener (Events.FreestyleEnd, HandleFreestyleEnd);
 	}
 
 	void HandleFreestyleTriggered() {
+		// jaja
+		StartCoroutine (Eh());
+	}
+
+	IEnumerator Eh() {
+		lyd.Play ();
+
+		yield return new WaitForSeconds (5);
+
+		lyd.Stop();
+	}
+
+	// funker ikke for øyeblikket
+	void HandleFreestyleEnd() {
+		lyd.Stop ();
 	}
 }
